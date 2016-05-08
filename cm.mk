6544e8d70c65fe75b621a57060af4c3e812e42c4
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-05-08 05:09:17 -0400

    pme: add missing makefiles
    Change-Id: I0b3ba35ae9cefe3d8b6cdac6269741f8a01daa5c

---
+++ cm.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/htc/pme/full_pme.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2560
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := pme
+PRODUCT_NAME := cm_pme
+PRODUCT_BRAND := HTC
+PRODUCT_MODEL := HTC 10
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_RELEASE_NAME := pme
